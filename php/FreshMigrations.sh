3f8ee379515c5755065b540853f60e4bd82aeeb2
Mohammad Nazari <mohammadnazari@nobody.invalid>
2020-07-31 12:40:19 +0430

    add two other sh command for composer

--- php/FreshMigrations.sh
+++ php/FreshMigrations.sh
@@ -1,2 +1,2 @@
-php /var/www/html/snappfood/artisan migrate:fresh --seed
-echo "* * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1" > /etc/crontabs/root
+cd /var/www/html/snappfood
+php artisan migrate:fresh --seed
---
+++ php/Prepration.sh
@@ -0,0 +1,4 @@
+cd /var/www/html/snappfood
+composer install
+php artisan migrate:fresh --seed
+echo "* * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1" > /etc/crontabs/root
