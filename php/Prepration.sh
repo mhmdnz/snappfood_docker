d5f4fee5ae9ce0c066453bca9559e7904221cbe7
Mohammad Nazari <mohammadnazari@nobody.invalid>
2020-07-31 15:29:38 +0430

    fix cron problem

--- php/Prepration.sh
+++ php/Prepration.sh
@@ -2,4 +2,6 @@ cd /var/www/html/snappfood
 composer install
 php artisan key:generate
 php artisan migrate:fresh --seed
-echo "* * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1" > /etc/crontabs/root
+cronjob="* * * * * cd /var/www/html/snappfood && php artisan schedule:run >> /dev/null 2>&1"
+(crontab -u root -l; echo "$cronjob" ) | crontab -u root -
+crond
